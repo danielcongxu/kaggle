86c34e96d8a51e0de0d6398cd5f458c63f0dd3ee
Cong Xu <danielxc29@163.com>
2018-04-18 23:48:04 +0800

    update

--- Toxic/toxic.py
+++ Toxic/toxic.py
@@ -9,6 +9,7 @@ from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import cross_val_score
 from sklearn import svm
+import xgboost as xgb
 
 # Loading data
 train_set = pd.read_csv("dataset/train.csv")
@@ -134,15 +135,13 @@ clf_rf = RandomForestClassifier(
 # ////////////////////////////////////////////////////////////////
 # GBDT
 clf_gb = GradientBoostingClassifier(
-    learning_rate=0.2,
-    n_estimators=300,
+    learning_rate=0.075,
+    n_estimators=800,
     max_depth=13,
     min_samples_split=0.015,
     min_samples_leaf=5,
     max_features=32,
-    subsample=0.75,
-    verbose=1
-)
+    subsample=0.75)
 
 # base line model
 # Accuracy : 0.9409
@@ -184,17 +183,104 @@ clf_gb = GradientBoostingClassifier(
 #                     scoring='roc_auc', n_jobs=8, method='GBDT')
 
 # refine-tune n_estimatosr
-param_grid = {"learning_rate": [0.1],
-              "n_estimators": [600]}
-misc.run_gridsearch(toxic_train_X, toxic_train_y, clf_gb, param_grid, sample_weight=True, cv=5,
-                    scoring='roc_auc', n_jobs=1, method='GBDT')
+# param_grid = {"learning_rate": [0.2],
+#               "n_estimators": [300]}
+# misc.run_gridsearch(toxic_train_X, toxic_train_y, clf_gb, param_grid, sample_weight=True, cv=5,
+#                     scoring='roc_auc', n_jobs=1, method='GBDT')
+#
+# param_grid = {"learning_rate": [0.1],
+#               "n_estimators": [600]}
+# misc.run_gridsearch(toxic_train_X, toxic_train_y, clf_gb, param_grid, sample_weight=True, cv=5,
+#                     scoring='roc_auc', n_jobs=1, method='GBDT')
+#
+# param_grid = {"learning_rate": [0.075],
+#               "n_estimators": [800]}
+# misc.run_gridsearch(toxic_train_X, toxic_train_y, clf_gb, param_grid, sample_weight=True, cv=5,
+#                     scoring='roc_auc', n_jobs=1, method='GBDT')
+#
+# param_grid = {"learning_rate": [0.05],
+#               "n_estimators": [1200]}
+# misc.run_gridsearch(toxic_train_X, toxic_train_y, clf_gb, param_grid, sample_weight=True, cv=5,
+#                     scoring='roc_auc', n_jobs=1, method='GBDT')
+#
+# param_grid = {"learning_rate": [0.04],
+#               "n_estimators": [1500]}
+# misc.run_gridsearch(toxic_train_X, toxic_train_y, clf_gb, param_grid, sample_weight=True, cv=5,
+#                     scoring='roc_auc', n_jobs=1, method='GBDT')
+#
+# param_grid = {"learning_rate": [0.03],
+#               "n_estimators": [2000]}
+# misc.run_gridsearch(toxic_train_X, toxic_train_y, clf_gb, param_grid, sample_weight=True, cv=5,
+#                     scoring='roc_auc', n_jobs=1, method='GBDT')
 
-param_grid = {"learning_rate": [0.05],
-              "n_estimators": [1200]}
-misc.run_gridsearch(toxic_train_X, toxic_train_y, clf_gb, param_grid, sample_weight=True, cv=5,
-                    scoring='roc_auc', n_jobs=1, method='GBDT')
+# With optimization, the optimal auc is 0.8751157067, the optimal accuracy is 0.945090275
+# clf_gb.fit(toxic_train_X, toxic_train_y)
+# score_auc = cross_val_score(clf_gb, toxic_train_X, toxic_train_y, cv=10, scoring='roc_auc').mean()
+# score_acc = cross_val_score(clf_gb, toxic_train_X, toxic_train_y, cv=10, scoring='accuracy').mean()
+# logger.info("After parameters tuning: average roc_auc is %.10f, average accuracy is %.10f" % (score_auc, score_acc))
+# ////////////////////////////////////////////////////////////////
 
-param_grid = {"learning_rate": [0.04],
-              "n_estimators": [1500]}
-misc.run_gridsearch(toxic_train_X, toxic_train_y, clf_gb, param_grid, sample_weight=True, cv=5,
-                    scoring='roc_auc', n_jobs=1, method='GBDT')
+# Xgboost
+clf_xgb = xgb.XGBClassifier(
+    learning_rate=0.1,
+    n_estimators=307,
+    max_depth=35,
+    min_child_weight=1,
+    gamma=0.1,
+    subsample=0.8,
+    colsample_bytree=0.8,
+    nthread=8,
+    scale_pos_weight=1)
+
+# misc.modelfit_xgboost(clf_xgb, toxic_train_X, toxic_train_y)
+# base line model
+# Accuracy : 0.9444
+# AUC Score (Train): 0.877955
+# misc.modelfit(clf_gb, toxic_train_X, toxic_train_y)
+
+# fine tune max_depth and min_child_weight(min_child_leaf)
+# optimal: max_depth: 15, min_child_weight: 1
+# param_grid = {"max_depth": np.arange(3, 16, 2),
+#                 "min_child_weight": np.arange(1, 10, 2)}
+#
+# misc.run_gridsearch(toxic_train_X, toxic_train_y, clf_xgb, param_grid, sample_weight=False, cv=5,
+#                     scoring='roc_auc', n_jobs=8, method='XGBoost')
+
+# refine-tune max_depth
+# optimal: max_depth: 35
+# param_grid = {"max_depth": [15, 16, 17],
+#               "min_child_weight": [1, 2]}
+#
+#  misc.run_gridsearch(toxic_train_X, toxic_train_y, clf_xgb, param_grid, sample_weight=False, cv=5,
+#                     scoring='roc_auc', n_jobs=8, method='XGBoost')
+#  param_grid = {"max_depth": [23, 25, 27, 29, 31, 33, 35, 37, 39]}
+#  misc.run_gridsearch(toxic_train_X, toxic_train_y, clf_xgb, param_grid, sample_weight=False, cv=5,
+#                     scoring='roc_auc', n_jobs=8, method='XGBoost')
+
+# fine tune gamma
+# optimal: gamma: 0.1
+# param_grid = {"gamma": np.arange(0, 0.5, 0.1)}
+# misc.run_gridsearch(toxic_train_X, toxic_train_y, clf_xgb, param_grid, sample_weight=False, cv=5,
+#                     scoring='roc_auc', n_jobs=8, method='XGBoost')
+
+# fine tune subsample and colsample_bytree
+if __name__ == "__main__":
+    param_grid = {"subsample": np.arange(0.6, 1, 0.1),
+                  "colsample_bytree": np.arange(0.6, 1, 0.1)}
+    ret = misc.run_gridsearch(toxic_train_X, toxic_train_y, clf_xgb, param_grid, sample_weight=False, cv=5,
+                              scoring='roc_auc', n_jobs=4, method='XGBoost')
+    opt_subsample = ret['subsample']
+    opt_colsubmple = ret['colsample_bytree']
+    clf_xgb.set_params(subsample=opt_subsample, colsample_bytree=opt_colsubmple)
+
+    param_grid = {"subsample": [opt_subsample - 0.05, opt_subsample, opt_subsample + 0.05],
+                  "colsample_bytree": [opt_colsubmple - 0.05, opt_colsubmple, opt_colsubmple+ 0.05]}
+    ret = misc.run_gridsearch(toxic_train_X, toxic_train_y, clf_xgb, param_grid, sample_weight=False, cv=5,
+                        scoring='roc_auc', n_jobs=4, method='XGBoost')
+
+    opt_subsample = ret['subsample']
+    opt_colsubmple = ret['colsample_bytree']
+    clf_xgb.set_params(subsample=opt_subsample, colsample_bytree=opt_colsubmple)
+    param_grid = {"reg_lambda": [1e-2, 0.1, 1, 10, 100]}
+    misc.run_gridsearch(toxic_train_X, toxic_train_y, clf_xgb, param_grid, sample_weight=False, cv=5,
+                        scoring='roc_auc', n_jobs=8, method='XGBoost')
